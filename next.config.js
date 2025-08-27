22d625f2aec67509c050b81de62cf2321a50b4b3
Mael <maeldjedid@gmail.com>
2025-08-27 18:24:30 +0200

    feat: optimize projects loading and update dependencies

--- next.config.js
+++ next.config.js
@@ -2,27 +2,26 @@
 const nextConfig = {
   // Active les optimisations
   swcMinify: true,
-  
+
   // Headers de cache pour les assets
   async headers() {
     return [
       {
-        source: '/:all*(svg|jpg|jpeg|png|gif|ico|webp)',
+        source: "/:all*(svg|jpg|jpeg|png|gif|ico|webp)",
         headers: [
           {
-            key: 'Cache-Control',
-            value: 'public, max-age=9999999999, must-revalidate',
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
           },
         ],
       },
-    ]
+    ];
   },
 
   // Optimisation expérimentales
   experimental: {
     optimizeCss: false,
   },
-}
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
